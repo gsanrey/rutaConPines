eba963cce31a8da477dda5f1e4bdff4d24a462e0
Gabriel Urso Santana Reyes <brielsantana@gmail.com>
2015-12-30 00:11:07 +0000

    Initial Commit

---
+++ rutaConPines/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rutaConPines
+//
+//  Created by Gabriel Urso Santana Reyes on 30/12/15.
+//  Copyright © 2015 Gabriel Urso Santana Reyes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
